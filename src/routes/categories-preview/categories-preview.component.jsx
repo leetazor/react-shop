d7b3bee4afeca802427ffcbd567fcfb850989799
unknown <Admin@GWD-SARA.gwd.local>
2022-06-15 12:36:45 +1000

    fixing bits

--- src/routes/categories-preview/categories-preview.component.jsx
+++ src/routes/categories-preview/categories-preview.component.jsx
@@ -5,16 +5,25 @@ import { selectCategoriesMap } from '../../store/categories/category.selector';
 
 import CategoryPreview from '../../components/category-preview/category-preview.component';
 
+import Spinner from '../../components/spinner/spinner.component';
+import { selectCategoriesIsLoading } from '../../store/categories/category.selector';
+
 const CategoriesPreview = () => {  
+    const isLoading = useSelector(selectCategoriesIsLoading);
     const categoriesMap = useSelector(selectCategoriesMap);
-    //const { categoriesMap } = useContext(CategoriesContext);
+  
     return (
       <Fragment> 
-        {Object.keys(categoriesMap).map(title => {
-          const products = categoriesMap[title];          
-          return <CategoryPreview key={title} title={title} products={products} />
-        })}     
-      </Fragment>      
+        { isLoading ? (
+        <Spinner />
+         ) : (Object.keys(categoriesMap).map(title => {
+            const products = categoriesMap[title];          
+              return (
+              <CategoryPreview key={title} title={title} products={products} />
+              );
+          }))
+        }
+      </Fragment>
     );
   };
 
